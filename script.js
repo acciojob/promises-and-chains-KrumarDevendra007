4788f3d343d22fadb8ea37a113fb31b9bc96de4c
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2024-12-18 23:22:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+const age = document.getElementById("age").value;
+const name = document.getElementById("name").value;
+const promise = new Promise((reslove, reject) => {
+	if(age > 18){
+		reslove(`Welcome ${name}. You can vote.`);
+	}
+	else{
+		reject(`Oh sorry ${name}. You aren't old enough.`);
+	}
+})

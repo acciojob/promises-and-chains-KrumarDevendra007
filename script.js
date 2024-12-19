d3f538699cc3f814a33958a50d328dc137ab1fb3
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2024-12-19 22:59:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 //your JS code here. If required.
+const form = document.getElementById("userForm");
 
-const age = document.getElementById("age").value;
-const name = document.getElementById("name").value;
-const promise = new Promise((reslove, reject) => {
-	if(age > 18){
-		reslove(`Welcome ${name}. You can vote.`);
+form.addEventListener("submit", (event) => {
+
+	const age = document.getElementById("age").value;
+    const name = document.getElementById("name").value;
+
+	const ageNum = parseInt(age, 10);
+
+	checkAge(name, age)
+		.then(message1 => {
+			console.log(message1);
+		})
+		.catch(message2 => {
+			console.log(message2);
+		});
+});
+
+const checkAge = () => {
+
+	return new Promise(resolve, reject){
+		setTimeOut(() => {
+			if(age > 18){
+				resolve(`Welcome, ${name}. You can vote.`);
+			}
+			else{
+				reject(`Oh sorry ${name}. You aren't old enough.`);
+			}
+		}, 4000);
 	}
-	else{
-		reject(`Oh sorry ${name}. You aren't old enough.`);
-	}
-})
+}
